7e3f6623d583fe8af7128f5d5925e30f2c16e34a
Wilwous <134493446+Wilwous@users.noreply.github.com>
2023-07-26 12:33:01 +0300

    Delete quiz-ios-1 directory

--- quiz-ios-1/MovieQuiz/Services/QuestionFactoryDelegate.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-protocol QuestionFactoryDelegate {
-    func didReceiveNextQuestion(question: QuizQuestion?)
-    func didLoadDataFromServer()
-    func didFailToLoadData(with error: Error)
-    func didFailToLoadImage(with error: Error)
-}
